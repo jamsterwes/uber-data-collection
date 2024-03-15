7fc42febc247717c86e8406f144b97e3cf85240b
Wesley Taylor <jamsterwes@gmail.com>
2024-03-15 14:07:17 -0500

    light mode smh

--- app/components/collection-form.tsx
+++ app/components/collection-form.tsx
@@ -4,11 +4,22 @@ import Image from "next/image";
 import { LocalTimeInput } from "@/app/components/local-time-input";
 
 export const CollectionForm = () => {
+  const textInput = "bg-stone-50 dark:bg-white text-stone-950 "
+
+  const uberButton = textInput + "w-56 h-12 border dark:border-black rounded-lg px-3 py-2 font-bold text-2xl flex gap-1"
+
+  const priceInput = textInput + "w-56 h-12 rounded-lg font-bold text-2xl grid grid-cols-[2rem_1fr] "
+  const priceSubPiece = "w-full h-12 border-y px-2 py-2 dark:border-black bg-stone-50 dark:bg-white "
+
+  const localTimeInput = textInput + "border dark:border-black w-56 h-12 py-5 px-1 rounded-lg font-bold text-md "
+
+  const submitButton = "bg-stone-50 dark:bg-white text-stone-950 border dark:border-black w-56 h-12 py-5 px-1 rounded-lg font-bold text-2xl flex items-center justify-center cursor-pointer"
+
   return <div className="flex flex-col gap-6">
       {/* Step 1 */}
       <section>
         <h2 className="text-xl font-semibold mb-0">Step 1 - Open ride in Uber (select UberX):</h2>
-        <a href="uber://..." target="_blank" className="bg-white w-56 h-12 rounded-lg text-black px-3 py-2 font-bold text-2xl flex gap-1">
+        <a href="uber://..." target="_blank" className={uberButton}>
           <Image src="https://upload.wikimedia.org/wikipedia/commons/7/79/Uber_App_Icon.svg" width={32} height={32} alt="Uber Logo" />
           Open in Uber
         </a>
@@ -16,25 +27,25 @@ export const CollectionForm = () => {
       {/* Step 2 */}
       <section>
         <h2 className="text-xl font-semibold mb-0">Step 2 - Enter ride price:</h2>
-        <span className="bg-white w-56 h-12 rounded-lg text-black font-bold text-2xl grid grid-cols-[2rem_1fr]">
-          <span className="w-4 px-2 py-2">$</span>
-          <input name="price" type="text" placeholder="0.00" className="w-full py-2 rounded-r-lg" />
+        <span className={priceInput}>
+          <span className={priceSubPiece + "border-l rounded-l-lg"}>$</span>
+          <input name="price" type="text" placeholder="0.00" className={priceSubPiece + "border-r rounded-r-lg"} />
         </span>
       </section>
       {/* Step 3 */}
       <section>
         <h2 className="text-xl font-semibold mb-0">Step 3 - Enter leave time (in CST):</h2>
-        <LocalTimeInput className="bg-white w-56 h-12 py-5 px-1 rounded-lg text-black font-bold text-md" tz="America/Chicago" autofill={true} />
+        <LocalTimeInput className={localTimeInput} tz="America/Chicago" autofill={true} />
       </section>
       {/* Step 4 */}
       <section>
         <h2 className="text-xl font-semibold mb-0">Step 4 - Enter arrive time (in CST):</h2>
-        <LocalTimeInput className="bg-white w-56 h-12 py-5 px-1 rounded-lg text-black font-bold text-md" tz="America/Chicago" />
+        <LocalTimeInput className={localTimeInput} tz="America/Chicago" />
       </section>
       {/* Step 5 */}
       <section>
         <h2 className="text-xl font-semibold mb-0">Step 5 - Submit ride:</h2>
-        <div className="bg-white text-black w-56 h-12 py-5 px-1 rounded-lg font-bold text-2xl flex items-center justify-center cursor-pointer">
+        <div className={submitButton}>
           Submit
         </div>
       </section>
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-stone-950 dark:bg-black dark:text-white">
       <body className={inter.className}>
         <div className="grid grid-rows-[72px_1fr]">
           <header className="h-full w-full flex items-center gap-2">
-            <Image src="/promenade.svg" alt="logo" width={32} height={32} className="aspect-square mx-2" />
-            <h1 className="text-3xl font-extrabold text-white">Promenade Team</h1>
+            <span>
+              <Image src="/promenade.svg" alt="logo-dark-theme" width={32} height={32} className="hidden dark:block aspect-square mx-2" />
+              <Image src="/promenade-black.svg" alt="logo-light-theme" width={32} height={32} className="block dark:hidden aspect-square mx-2" />
+            </span>
+            <h1 className="text-3xl font-extrabold">Promenade Team</h1>
           </header>
           <main className="h-full w-full">
             {children}
