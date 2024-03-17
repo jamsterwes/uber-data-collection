f419cf3299d921258901a131b2ebc0f311bccc3f
Wesley Taylor <jamsterwes@gmail.com>
2024-03-17 16:03:55 -0500

    add ability to upload locations

--- actions/locations.ts
+++ actions/locations.ts
@@ -2,6 +2,8 @@
 
 import locations from "@/data/locations"
 
+import { parse } from "csv-parse/sync"
+
 export async function getLocations(start: number = 0, maxCount: number = 100) {
     // Return data
     return locations.slice(start, maxCount);
@@ -11,7 +13,21 @@ export async function uploadLocations(data: FormData) {
     // Get the file of locations
     const csvFile = data.get("csvFile") as File;
 
-    // Print location data
-    const locationData = await csvFile.text();
-    console.log(locationData.slice(0, 50));
+    // Get location data
+    const locationDataText = await csvFile.text();
+
+    // Parse CSV
+    const records = parse(locationDataText, {
+        columns: true
+    });
+    const locations: Location[] = records.map(({id, latitude, longitude}: {
+        id: string,
+        latitude: string,
+        longitude: string
+    }) => {
+        return {id: BigInt(id), latitude: parseFloat(latitude), longitude: parseFloat(longitude)}
+    });
+
+    // Print a location
+    console.log(locations[0]);
 }
