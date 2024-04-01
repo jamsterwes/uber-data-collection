8c1d8bfe723b789c59d0a919a9b22a67bd188815
Wesley Taylor <jamsterwes@gmail.com>
2024-04-01 10:28:47 -0500

    installed kysely

---
+++ data/db.ts
@@ -0,0 +1,44 @@
+import { createKysely } from '@vercel/postgres-kysely';
+
+import {
+    Generated,
+    Insertable,
+    Selectable,
+    Updateable
+  } from 'kysely'
+
+export interface Database {
+    location: LocationTable
+    ride: RideTable
+}
+
+export interface LocationTable {
+    id: Generated<number>
+    latitude: number
+    longitude: number
+}
+
+export interface RideTable {
+    id: Generated<number>
+    start_latitude: number
+    start_longitude: number
+    end_latitude: number
+    end_longitude: number
+    collected: boolean
+    time: number | null
+    distance: number | null
+    price: number | null
+}
+
+// Location exports
+export type Location = Selectable<LocationTable>
+export type NewLocation = Insertable<LocationTable>
+
+// Ride exports
+export type Ride = Selectable<RideTable>
+export type NewRide = Insertable<RideTable>
+export type RideUpdate = Updateable<RideTable>
+
+// Export db
+const db = createKysely<Database>()
+export default db
